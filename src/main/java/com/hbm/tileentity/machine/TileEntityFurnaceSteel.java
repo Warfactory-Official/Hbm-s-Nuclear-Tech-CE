17cbd68b9fe0b794264db0a705e1ae1d89ad38a5
TheSlize <r.region-4@yandex.ru>
2026-09-12 03:50:15 +0300

    fix: #1667, #1656, #1663
    - adapted rubber boat texture to 1.12 format
    - changed welded bscco coils <-> fusion blanket locales
    - fixed an NPE regarding conveyor inserter+steel furnace

--- src/main/java/com/hbm/tileentity/machine/TileEntityFurnaceSteel.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityFurnaceSteel.java
@@ -42,7 +42,7 @@ public class TileEntityFurnaceSteel extends TileEntityMachineBase implements IGU
 	public int heat;
 	public static final int maxHeat = 100_000;
 	public static final double diffusion = 0.05D;
-	private final ItemStack[] lastItems = new ItemStack[3];
+	private final ItemStack[] lastItems = new ItemStack[] {ItemStack.EMPTY, ItemStack.EMPTY, ItemStack.EMPTY};
 	
 	public boolean wasOn = false;
 	    
